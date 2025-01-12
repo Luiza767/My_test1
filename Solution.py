9c7314643c285decd304cf6a0392098e2cba8a38
Luiza767 <parostok.root@seznam.cz>
2025-01-12 11:04:46 +0100

    Translate

---
+++ Solution.py
@@ -0,0 +1,17 @@
+
+
+print ('Program for reading numbers from right to left')
+n = int(input('enter the number n -> '))
+print('Number from right to left:', end = ' ')
+k = 0
+n2 = n
+while n2 != 0:
+    n2 //= 10
+    k += 1
+
+n3 = 0
+for i in range(k - 1, -1, -1):
+    a = n % 10
+    n3 = n3 + a * 10 ** i
+    n //= 10
+print(n3)
